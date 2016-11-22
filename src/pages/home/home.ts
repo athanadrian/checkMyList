c97924053dfc3b5757263b3b490d57d53b5cc958
Athanasios Andrianesis <Athanasios Andrianesis>
2016-11-22 12:03:54 +0200

    home page functions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController, AlertController, Platform } from 'ionic-angular';
 
+import { ChecklistPage } from '../checklist/checklist';
 import { DataService } from '../../providers/data-service';
 import { ChecklistModel } from '../../models/checklist-model';
 
@@ -20,27 +21,91 @@ export class HomePage {
 
   }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
 
   }
 
-  addChecklist(){
+  //Add button
+  addChecklist(): void {
     console.log('Adding checklist.....');
+    let alert = this.alertController.create({
+      title: 'Νέα λίστα',
+      message: 'Δώστε όνομα για τη νέα λίστα:',
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Άκυρο'
+        },
+        {
+          text: 'Αποθήκευση',
+          handler: data => {
+            let newChecklist = new ChecklistModel(data.name, []);
+            this.checklists.push(newChecklist);
+
+            newChecklist.checklist.subscribe(update => {
+              this.save();
+            });
+            this.save();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
-  renameChecklist(checklist){
+  //edit sliding button
+  renameChecklist(checklist): void {
     console.log('Renaming checklist.....');
+    let alert = this.alertController.create({
+      title: 'Αλλαγή ονόματος λίστας',
+      message: 'Δώστε νέο όνομα για τη λίστα:',
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Άκυρο'
+        },
+        {
+          text: 'Αποθήκευση',
+          handler: data => {
+            let index = this.checklists.indexOf(checklist);
+
+            if (index > -1) {
+              this.checklists[index].setTitle(data.name);
+              this.save();
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
-  viewChecklist(checklist){
+  //list button
+  viewChecklist(checklist): void {
     console.log('Viewing checklist.....');
+    this.navCtrl.push(ChecklistPage, { checklist: checklist });
   }
 
-  removeChecklist(checklist){
+  //delete sliding button
+  removeChecklist(checklist): void {
     console.log('Removing checklist.....');
+    let index = this.checklists.indexOf(checklist);
+
+    if (index > -1) {
+      this.checklists.splice(index, 1);
+      this.save();
+    }
   }
 
-  save():void{
+  save(): void {
     console.log('Checklist saved.....');
   }
 }
